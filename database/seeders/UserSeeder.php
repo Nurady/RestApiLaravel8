5d44adede89653128940e3846433fd5398c891a9
unknown <nursyafriady83@gmail.com>
2021-12-15 23:07:40 +0800

    Role

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
@@ -13,11 +15,44 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::create([
-            'name' => 'adis',
-            'email' => 'adis@gmail.com',
-            'password' => bcrypt('12345678'),
-            'email_verified_at' => now()
-        ]);
+        // \App\Models\User::create([
+        //     'name' => 'adis',
+        //     'email' => 'adis@gmail.com',
+        //     'password' => bcrypt('12345678'),
+        //     'email_verified_at' => now()
+        // ]);
+
+        collect([
+            [
+                'name' => 'adis2',
+                'email' => 'adis2@gmail.com',
+                'password' => bcrypt('12345678'),
+                'email_verified_at' => now()
+            ],
+            [
+                'name' => 'yadi',
+                'email' => 'yadi@gmail.com',
+                'password' => bcrypt('12345678'),
+                'email_verified_at' => now()
+            ],
+            [
+                'name' => 'izzan',
+                'email' => 'izzan@gmail.com',
+                'password' => bcrypt('12345678'),
+                'email_verified_at' => now()
+            ]
+        ])->each(function($user) {
+            User::create($user);
+        });
+
+        collect([
+            'admin',
+            'moderator'
+        ])->each(function($role) {
+           Role::create(['name' => $role]);
+        });
+
+        User::find(1)->roles()->attach([1]);
+        User::find(2)->roles()->attach([2]);
     }
 }
