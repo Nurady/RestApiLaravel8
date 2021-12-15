8c3f669787d8cd6792befa3788ca299e81ea5c39
unknown <nursyafriady83@gmail.com>
2021-12-15 21:44:38 +0800

    delete product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -94,6 +94,10 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        $product->delete();
+
+        return response()->json([
+            'message' => 'success deleted product',
+        ]);
     }
 }
